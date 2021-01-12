2e1ed46a228e792887ba19521ea0edb776bb5bd0
Dani Ihza Farrosi <dani.videda@gmail.com>
2021-01-12 16:20:35 +0700

    Initial connect Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-// ignore: unused_import
+import 'package:firebase_core/firebase_core.dart';
 import 'views/homepage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
