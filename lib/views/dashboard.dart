6fda6037d0fcb0ddadc568d060deaea2d9de20be
Dani Ihza Farrosi <dani.videda@gmail.com>
2021-01-12 12:01:41 +0700

    add layout gridview for dashboard

---
+++ lib/views/dashboard.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  List<String> textList = ["text 1", "text 2", "text 3", "text 4", "text 5"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'Projects',
+          style: TextStyle(
+            fontFamily: 'Poppins',
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.all(Radius.circular(10))
+              ),
+              child: Center(child: Text(textList[index])),
+            ),
+          );
+        },
+        itemCount: 5,
+      ),
+    );
+  }
+}
